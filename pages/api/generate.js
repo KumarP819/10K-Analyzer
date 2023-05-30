0ef35f80f7d2acd815f3216f531826164f99e379
Kumar Pulivarthi <kumarpulivarthi@Kumars-Air.ht.home>
2023-05-29 20:49:37 -0400

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 
+`
+Write a summary of the 10K file given in a way that explains in a list the top 3-5 risks of the company. Then add a section and talk about competitors and alternatives for the business, and finally, a projected future of the company(successful or not successful) 
+
+Link: 
+`
+;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
